c0562b3a10b481d4b905baa3f1d0f2d90105860e
mehrawbmh <mehrabmh81@yahoo.com>
2023-12-29 19:23:57 +0330

    feat: add command manager

---
+++ headers/command_manager.hpp
@@ -0,0 +1,30 @@
+#ifndef SPUTIFY_COMMAND_MANAGER_HPP
+#define SPUTIFY_COMMAND_MANAGER_HPP
+
+using namespace std;
+
+#include <iostream>
+#include <string>
+#include "users_controller.hpp"
+
+enum class HttpMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+};
+
+const string SIGNUP_COMMAND = "signup";
+
+class CommandManager {
+private:
+    static HttpMethod getRequestMode(string methodInput);
+
+public:
+    CommandManager() = default;
+
+    static void handle(int argc, char* argv[]);
+};
+
+
+#endif
---
+++ src/command_manager.cpp
@@ -0,0 +1,58 @@
+#include "../headers/command_manager.hpp"
+#include "../headers/client_exception.hpp"
+
+HttpMethod CommandManager::getRequestMode(string methodInput) {
+    if (methodInput == "POST") {
+        return HttpMethod::POST;
+    } else if (methodInput == "GET") {
+        return HttpMethod::GET;
+    } else if (methodInput == "PUT") {
+        return HttpMethod::PUT;
+    } else if (methodInput == "DELETE") {
+        return HttpMethod::DELETE;
+    } else {
+        throw ClientException(400);
+    }
+}
+
+void CommandManager::handle(int argc, char* argv[]) {
+    string questionMark = argv[3];
+    if ((argc < 4) || (questionMark != "?")) {
+        throw ClientException(400);
+    }
+
+    HttpMethod method = getRequestMode(argv[1]);
+    
+    if (method == HttpMethod::POST && argv[2] == SIGNUP_COMMAND) {
+        if (argc < 8) {
+            throw ClientException(400);
+        }
+
+        UsersController controller;
+        string userName, passWord;
+        string firstArg = argv[4];
+        string secondArg = argv[6];
+        if (firstArg == "username") {
+            userName = argv[5];
+            if (secondArg == "password") {
+                passWord = argv[7];
+            } else {
+                throw ClientException(400);
+            }
+        } else if (secondArg == "username") {
+            userName = argv[7];
+            if (firstArg == "password") {
+                passWord = argv[5];
+            } else {
+                throw ClientException(400);
+            }
+        } else {
+            throw ClientException(400);
+        }
+        
+        return controller.signUp(userName, passWord);
+    }
+
+    cout << "INVALID COOMMAND\n";
+    throw ClientException(400);
+}
