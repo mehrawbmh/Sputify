11fc9ffae8822f970d8ec2b8891afa150f1d411b
mehrab <mehrabmh81@yahoo.com>
2023-12-31 12:24:43 +0330

    add music controller

--- headers/musics_controller.hpp
+++ headers/musics_controller.hpp
@@ -10,6 +10,12 @@ private:
     Database* db;
 public:
     MusicsController(Database* db);
+
+    void createMusic(string title, string path, string album, int year, string durationTime, vector<string> tags);
+
+    void getOneMusic(int id);
+
+    void getAllMusics();
 };
 
 #endif // SPUTIFY_MUSICS_CONTROLLER_HPP
--- src/musics_controller.cpp
+++ src/musics_controller.cpp
@@ -1,4 +1,39 @@
 #include "../headers/musics_controller.hpp"
+#include "../headers/musics_model.hpp"
+#include "../headers/view.hpp"
 
+MusicsController::MusicsController(Database *_db) : db(_db) {}
 
-MusicsController::MusicsController(Database *_db): db(_db) {}
+void MusicsController::createMusic(string title, string path, string album, int year, string durationTime, vector<string> tags)
+{
+    MusicsModel model = MusicsModel(db);
+    View view;
+    
+    cout << "here before model\n";
+    int result = model.addNewMusic(title, path, album, year, durationTime, tags);
+    cout << view.showResponse(result) << endl;
+}
+
+void MusicsController::getOneMusic(int id) {
+    MusicsModel model = MusicsModel(db);
+    View view;
+
+    if (this->db->getCurrentUser() == nullptr) {
+        cout << view.showResponse(STATUS_403_FORBIDDEN);
+    }
+
+    cout << "getting one music...\n";
+    cout << view.showMusicDetail(model.getOneMusic(id)) << endl;
+}
+
+void MusicsController::getAllMusics() {
+    MusicsModel model = MusicsModel(db);
+    View view;
+
+    if (this->db->getCurrentUser() == nullptr) {
+        cout << view.showResponse(STATUS_403_FORBIDDEN);
+    }
+
+    cout << "getting all musics...\n";
+    cout << view.showMusicsList(model.getAllMusics());
+}
